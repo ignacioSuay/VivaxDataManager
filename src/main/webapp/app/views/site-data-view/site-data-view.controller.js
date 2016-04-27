3749d9a7105f2cbf31a06a83f23b9b2c179dbdb5
ignacio.suay <ignacio.suay@gmail.com>
2016-04-27 16:45:12 +0100

    Create multiple filter dropdown

--- src/main/webapp/app/views/site-data-view/site-data-view.controller.js
+++ src/main/webapp/app/views/site-data-view/site-data-view.controller.js
@@ -9,63 +9,53 @@
     console.log('in controller function')
     function SiteDataViewController ($scope, $state, SiteDataViewDTO, SiteDataSearch) {
 
-       $scope.filters = [{name:'', query:'' }];
-
-         $scope.addFilter= function(){
-             $scope.filters.push({});
-         };
-
+        //$scope.names = [ 'Study Type', 'Study Ref', 'Category', 'Country', 'Year Start', 'Year End', 'Upper95CI', 'PubMedId', 'Treatments' ];
+        $scope.names = [
+            {text: "Study Type", type: "text"},
+            {text: "Study Ref", type: "text"},
+            {text: "Category", type: "text"},
+            {text: "Country", type: "text"},
+            {text: "Year Start", type: "text"},
+            {text: "Year End", type: "text"},
+            {text: "Upper95CI", type:"text"},
+            {text: "PubMedId", type:"text"},
+            {text: "Treatments", type:"text"}];
+        $scope.filters = [{}];
         $scope.siteDataViewDTOS = [];
-        $scope.loadAll = function() {
-            var data = [{name:'country', query:'China'}];
-            $scope.siteDataViewDTOS = SiteDataViewDTO.query(data);
+
+        $scope.addFilter = function () {
+            $scope.filters.push({});
         };
 
+        $scope.removeFilter = function (index) {
+            $scope.filters.splice(index, 1);
+        };
+
+        $scope.loadAll = function () {
+            var data = [{name: 'country', query: 'China'}];
+            $scope.siteDataViewDTOS = SiteDataViewDTO.query(data);
+        };
+        $scope.loadAll();
+
         $scope.search = function () {
-            if (!$scope.searchQuery) {
-                return $scope.loadAll();
-            }
-            SiteDataSearch.query({query: $scope.searchQuery}, function(result) {
+            //if (!$scope.searchQuery) {
+            //    return $scope.loadAll();
+            //}
+            alert(JSON.stringify($scope.filters));
+            SiteDataSearch.query({query: $scope.searchQuery}, function (result) {
                 $scope.siteDataViewDTOS = result;
             });
         };
 
-        $scope.loadAll();
-
-        $scope.names = [ 'Study Type', 'Study Ref', 'Category', 'Country', 'Year Start', 'Year End', 'Upper95CI', 'PubMedId', 'Treatments' ];
-        
-        $scope.status = {
-            isopen : false
-        };
-
-        $scope.toggled = function(open) {
-            console.log('Dropdown is now: ', open);
-        };
-
-        $scope.toggleDropdown = function($event) {
-            $event.preventDefault();
-            $event.stopPropagation();
-            $scope.status.isopen = !$scope.status.isopen;
-        };
-
-        $scope.filters = [{name:'Filters', query:'' }];
-
-        $scope.pickFilterName = function(nameBody){
-          $scope.name = nameBody;
-          console.log($scope.name);
-        };
-
-        $scope.pickFilterQuery = function(queryBody){
+        $scope.pickFilterQuery = function (queryBody) {
             console.log($scope.name);
             $scope.query = queryBody;
-            var filter = {name:$scope.name, query:$scope.query };
+            var filter = {name: $scope.name, query: $scope.query};
             $scope.filters.push(filter);
             alert(JSON.stringify($scope.filters));
         };
 
-        $scope.appendToEl = angular.element(document
-			.querySelector('#dropdown-long-content'));
-     }
+    }
 
 
 })();
